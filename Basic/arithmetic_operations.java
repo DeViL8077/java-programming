d94c6e21e93d4dea16be9cdd6237214872629228
Mann Patel <mann6657@gmail.com>
2025-01-25 10:16:07 +0000

    Programs

---
+++ Basic/arithmetic_operations.java
@@ -0,0 +1,19 @@
+public class arithmetic_operations {
+    public static void main(String[] args) {
+        int a=9;
+        int b=13;
+        int c;
+        c=a+b;
+        System.out.println(c);
+        c=a-b;
+        System.out.println(c);
+        c=a*b;
+        System.out.println(c);
+        c=a/b;
+        System.out.println(c);
+        c=a%b;
+        System.out.println(c);
+        
+    }
+            
+}
